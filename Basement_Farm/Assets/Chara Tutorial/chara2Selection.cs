aa5b57350c5062c510147c035c2881d435374bf1
GuniruPanjan <135081686+GuniruPanjan@users.noreply.github.com>
2023-07-19 18:32:46 +0900

    tyu-torial

---
+++ Basement_Farm/Assets/Chara Tutorial/chara2Selection.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chara2Selection : MonoBehaviour
+{
+    [SerializeField] private GameObject a = GameObject.Find("Canvas");  //GameObject型の変数aを宣言　好きなゲームオブジェクトをアタッチ
+
+    //UIボタンを非表示にする
+    void Start()
+    {
+        a.SetActive(false);
+    }
+    //オブジェクトクリックで選んだキャラ以外が消失
+    public void OnTouched()
+    {
+        //選ばれなかったキャラを宣言
+        GameObject obj1 = GameObject.Find("キャラ１");
+        GameObject obj2 = GameObject.Find("花");
+
+        Debug.Log("初めてのモンスターだ！");
+
+        //選ばれなかったキャラを破壊
+        Destroy(obj1);
+        Destroy(obj2);
+
+        //UIボタンを表示する
+        a.SetActive(true);
+    }
+}

2b95eca9afc952a95e455d8d26781c56c66ada89
GuniruPanjan <135081686+GuniruPanjan@users.noreply.github.com>
2023-07-26 18:20:20 +0900

    kuso

--- Basement_Farm/Assets/CharaBox/BoxChara3.cs
+++ Basement_Farm/Assets/CharaBox/BoxChara3.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SocialPlatforms.GameCenter;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class BoxChara3 : MonoBehaviour

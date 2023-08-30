e9feb1ba5aad29c0dcc017cd40ff87d4ec0307a7
f8yato <136441181+f8yato@users.noreply.github.com>
2023-08-30 17:00:23 +0900

    up

---
+++ Basement_Farm/Assets/home/Haken/Char006Reduce.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Char006Reduce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+        public void ReduceClick()
+    {
+        Chara006.CharCount006 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
